e45cccabeba10bc8c18e7498a2ad74e812597d35
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-09 01:47:27 -0400

    Head tracking header

--- include/head_tracker.h
+++ include/head_tracker.h
@@ -6,7 +6,9 @@
 /*
   Will locate the x and y coordinates of the head the head using background and current depth data from the kinect.
   Pointers `background` and `current` point to arrays in column major format. current[y][x] == current [y*width+x].
+  @param background A pointer to a snapshot of the depth of the background.
+  @param current A pointer to current depth array
 */
-void locate_head(const uint16_t *background, const uint16_t *current, int width, int height, int *x, int *y);
+void locate_head(const uint16_t *background, const uint16_t *current, int width, int height, int *x, int *y, uint16_t *z);
 
 #endif
---
+++ source/head_tracker.c
@@ -0,0 +1,6 @@
+#include "head_tracker.h"
+
+void locate_head(const uint16_t *background, const uint16_t *current, int width, int height, int *x, int *y, uint16_t z) {
+  *x = 0;
+  *y = 0;
+}
