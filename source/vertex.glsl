36ec410585b7a9654acee5bf6172fc198a865bb3
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-09 01:49:21 -0400

    Just a quick change

--- source/vertex.glsl
+++ source/vertex.glsl
@@ -1,11 +1,6 @@
 #version 330 core
 layout (location = 0) in vec3 position;
-uniform mat4 m = mat4(
-  0.5,0.0,0.0,0.04,
-  0.0,0.5,0.0,0.0,
-  0.0,0.0,0.5,0.2,
-  0.0,0.0,0.0,1.0
-);
+uniform mat4 m;
 void main() {
-  gl_Position = m * vec4(position.x, position.y, position.z, 1.0);
+  gl_Position = m * vec4(position, 1.0);
 }
