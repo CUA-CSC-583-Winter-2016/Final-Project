e1f59650f98e04e28d307ebb2a3767b0d11c01b3
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-10 23:32:31 -0400

    declared functions windowing and opengl utilities

---
+++ include/opengl_window.h
@@ -0,0 +1,25 @@
+#ifndef OPENGL_WINDOW_H
+#define OPENGL_WINDOW_H
+
+/*
+  Creates an opengl window and context.
+*/
+void create_window();
+
+/*
+  Checks if the window should be closed.
+  @return true when window should be closed.
+*/
+bool should_close_window();
+
+/*
+  Swaps the drawing buffer.
+*/
+void swap_buffers();
+
+/*
+  closes and the window destroyes the opengl context.
+*/
+void close_window();
+
+#endif
