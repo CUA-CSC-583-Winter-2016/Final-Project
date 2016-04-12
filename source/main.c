5d0002f144de802214d1ca2037045ce3ccfa3c1d
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-12 00:38:34 -0400

    Progressing on opengl_window.c

--- source/main.c
+++ source/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <opengl_window.h>
-#include <opengl_objects.h>
-#include <kinect_interface.h>
-#include <head_tracker.h>
-#include <matrix_math.h>
+#include "opengl_window.h"
+#include "opengl_objects.h"
+#include "kinect_interface.h"
+#include "head_tracker.h"
+#include "matrix_math.h"
+
+int actually_do_something(int argc, const char *argv[]) {
+  create_window();
+  return 0;
+}
 
 int main (int argc, const char *argv[]) {
   printf("Good news everyone, it's compiling!\n"); //TODO remove futurama reference and actually glew all the code together.
+  return 0;
 }
--- source/opengl_window.c
+++ source/opengl_window.c
@@ -1,13 +1,40 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
-void create_window() {
+GLFWwindow *window;
 
+void error_callback(int error, const char* description)
+{
+    printf("Error: %s\n",description);
 }
 
-void swap_buffers() {
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+  if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
+int create_window() {
+  if (!glfwInit())
+    return 1;
+  glfwSetErrorCallback(&error_callback);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  window = glfwCreateWindow(640, 480, "Off-Axis Perspective Box", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
+  glewExperimental = GL_TRUE;
+  glewInit(); // HAS TO BE CALLED AFTER THE WINDOW IS CREATED! WHY DID NO ONE TELL ME THIS!!!
+  return 0;
+}
+
+
+void swap_buffers() {
+  glfwSwapBuffers(window);
 }
 
 void close_window() {
-  
+  glfwDestroyWindow(window);
 }
