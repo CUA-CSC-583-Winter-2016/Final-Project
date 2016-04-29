60493254e1d25f0891fe5614c958a4225cdfbb6f
michaellmonaghan <michaellmonaghan@gmail.com>
2016-04-29 09:51:18 -0400

    Head tracker improvements and kinect interface implemented.

--- source/head_tracker.c
+++ source/head_tracker.c
@@ -2,15 +2,16 @@
 #include "stdlib.h"
 #define M_PI 3.14159265358979323846
 
+// Tweakables
+float A;
+
 /*
   Just a note,
   Certain numbers represent errors where the depth can't be determined.
   These numbers will almost certainly exist in both the `background` and `current` arrays.
-
-  Error numbers for convienience ;)
-
 */
 
+  //Error numbers for convienience ;)
   //Maximum value that a uint16_t pixel will take on in the buffer of any of the FREENECT_DEPTH_11BIT, FREENECT_DEPTH_10BIT, FREENECT_DEPTH_11BIT_PACKED, or FREENECT_DEPTH_10BIT_PACKED frame callbacks
   #define FREENECT_DEPTH_RAW_MAX_VALUE 2048
 
@@ -21,20 +22,30 @@ void locate_head(const uint16_t *background, const uint16_t current[], int width
 
   int x;
   int y;
-  uint16_t f = FREENECT_DEPTH_RAW_MAX_VALUE;
-  // The +- 1 offset buisness in the for loops makes sure we can always see the crosshairs.
-  for (y = 100; y < height-100; y++) {
-    for (x = 100; x < width-100; x++) {
-      uint16_t difference = current[x + y*width] - background[x + y*width];
-      //if (abs(difference) < 10) {
-      // For debuging purposes and just for fun, I've modified this code to locate the closest point to the kinect.
-      if (current[x + y*width] < f) {
-        f = current[x + y*width];
-        *headx = x;
-        *heady = y;
-        //return;
+  // The offset in the for loops makes sure we can always see the crosshairs.
+  for(y = 100; y < height-100; y++) {
+    for(x = 100; x < width-100; x++) {
+      if(current[x + y*width] != FREENECT_DEPTH_RAW_NO_VALUE && (current[x + y*width] - background[x + y*width]) > 100) {
+        int yc = y - (int)(A * current[x+y*width]);
+        int xl=x,xr=x;
+        for(int xltest = x; x > yc*width; x--) {
+          if(current[xltest + yc*width] == FREENECT_DEPTH_RAW_NO_VALUE || (current[xltest + yc*width] - background[xltest + yc*width]) > 100) {
+            xl = xltest;
+            break;
+          }
+        }
+        for(int xrtest = x; x > yc*width; x--) {
+          if(current[xrtest + yc*width] == FREENECT_DEPTH_RAW_NO_VALUE || (current[xrtest + yc*width] - background[xrtest + yc*width]) > 100) {
+            xr = xrtest;
+            break;
+          }
+        }
+        int xc = xl+xr/2;
+        *headx = xc;
+        *heady = yc;
+        *headz = current[*headx + *heady*width];
+        return;
       }
     }
   }
-  *headz = current[*headx + *heady*width];
 }
--- source/kinect_interface.c
+++ source/kinect_interface.c
@@ -4,15 +4,35 @@
 #include <stdio.h>
 #define M_PI 3.14159265358979323846
 
-int init_kinect() {
-  return 0;
+
+freenect_context *f_ctx;
+freenect_device *f_dev;
+
+void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp) {
+  // Just an empty callback
 }
-float depth_rotation = 0;
-void get_depth(uint16_t *buffer) {
-  for (int i = 0; i < 640*480; i++) {
-    buffer[i] = 2000+1000*sin(depth_rotation);
+
+int init_kinect() {
+  //init kinect
+  freenect_init(&f_ctx,NULL);
+  freenect_select_subdevices(f_ctx, (freenect_device_flags)(FREENECT_DEVICE_CAMERA));
+  if (!freenect_num_devices(f_ctx)) {
+    printf("No devices found\n");
+    freenect_shutdown(f_ctx);
+    return 1;
   }
-  depth_rotation += .05;
-  if(depth_rotation > 2 * M_PI)
-    depth_rotation -= 2 * M_PI;
+  freenect_open_device(f_ctx,&f_dev,0);
+  freenect_frame_mode fm = freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM,FREENECT_DEPTH_11BIT);
+  printf("width:\t%i\nheight:%i\nbytes:%i\n",fm.width,fm.height,fm.bytes);
+  freenect_set_depth_mode(f_dev,fm);
+  freenect_set_depth_callback(f_dev, depth_cb);
+  freenect_start_depth(f_dev);
+
+}
+
+void get_depth(uint16_t *buffer) {
+  freenect_set_depth_buffer(f_dev,buffer);
+  if(freenect_process_events(f_ctx) < 0) {
+    return;
+  }
 }
